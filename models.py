67c3fc21ca61366b5a9e455a4cdf34e611debf26
HOKAGE888 <gozhevaKatya@yandex.ru>
2024-03-14 19:24:51 +0300

    добавлены модели

---
+++ models.py
@@ -0,0 +1,49 @@
+from peewee import SqliteDatabase, Model, TextField, ForeignKeyField
+
+db = SqliteDatabase('db.db')
+
+
+class Table(Model):
+    
+    class Meta:
+        database = db
+
+
+class MetalType(Table):
+    name = TextField()
+
+
+class Brand(Table):
+    name = TextField()
+
+
+class ProductType(Table):
+    name = TextField()
+
+
+class ProductSubtype(Table):
+    name = TextField()
+    product_type = ForeignKeyField(ProductType)
+
+
+class Product(Table):
+    product_subtype = ForeignKeyField(ProductSubtype)
+    brand = ForeignKeyField(Brand)
+    metal_type = ForeignKeyField(MetalType)
+
+
+class Template(Table):
+    json = TextField()
+    imagemagick = TextField()
+    path_to_background = TextField()
+
+
+class Image(Table):
+    path = TextField()
+    product = ForeignKeyField(Product)
+    template = ForeignKeyField(Template)
+
+
+db.connect()
+db.create_tables([MetalType, Brand, ProductType, ProductSubtype, Product, Template, Image])
+db.close()
