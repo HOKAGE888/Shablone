2e69986b569f8296e4206047d46f93394ad427c3
Jost221 <mr.z.75@mail.ru>
2024-03-21 09:15:50 +0300

    fix css bug and add start add text title

--- static/edit_template.js
+++ static/edit_template.js
@@ -2,14 +2,17 @@ template_pattern = [
     {
         "id": "1",
         "style": {
-            "margin-left": "500px",
+            
             "z-index": "1",
-            "margin-top": "50px",
             "position": "absolute",
             "width": "600px",   
             "height": "800px",
             "background-color": "#ffffff",
         },
+        "class": "for-js-background",
+        "const": {
+
+        }
     },
     {
         "id": "2",
@@ -52,11 +55,18 @@ function add_element(element) {
         for (const [key, value] of Object.entries(template_pattern[index])) {
             if (typeof (value) == 'string') {
                 innerElement += `${key}="${value}"`;
+            } else if (key == "const") {
+                
             } else {
                 if (key == "style") {
                     innerElement += 'style="'
                     for (const [key_style, value_style] of Object.entries(template_pattern[index][key])) {
-                        innerElement += `${key_style}: ${value_style};  `;
+                        if ("const" in template_pattern && key_style in template_pattern["const"]){
+                            innerElement += `${key_style}: calc(${value_style} + ${template_pattern["const"][key_style]});  ` 
+                        } else {
+                          innerElement += `${key_style}: ${value_style};  `;  
+                        }
+                        
                     }
                     innerElement += '"'
                 }
@@ -70,7 +80,27 @@ function add_element(element) {
 
 /* <a href="https://www.flaticon.com/ru/free-icons/-image"</a> */
 
-
+id = 0
 window.onload = function () {
     update_template_view()
+    document.getElementById("add-text").addEventListener('click', function(){
+            id+=1
+            template_pattern.push(
+                {
+                    "id":String(id),
+                    "style": {
+                        "background-color": "#dadada",
+                        "width": "100px",
+                        "height": "100px",
+                        "margin-top": "0px",
+                        "margin-left": "0px"
+                    },
+                    "const":{
+                        "margin-top": `${(document.documentElement.scrollWidth-100)/2}px`,
+                        "margin-left": `${(document.documentElement.scrollWidth-100)/2}px`,
+                    }
+                }
+            )
+        add_element()
+    })
 }
