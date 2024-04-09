dde78ce10b127f451ad8645952b0f78132c6707c
HOKAGE888 <gozhevaKatya@yandex.ru>
2024-04-09 16:29:29 +0300

    создание шпблога из гуи, пример создания, редактирования, перетаскивавния текста

--- static/po.js
+++ static/po.js
@@ -1,11 +1,3 @@
-// МОДАЛЬНОЕ ОКНО
-
-// Код для обработки нажатия на кнопку открытия модального окна
-document.getElementById('openModalBtn').addEventListener('click', function() {
-    document.getElementById('myModal').style.display = 'block';
-});
-
-
 // Функция для загрузки данных JSON из API
 function fetchData(url, callback) {
     fetch(url)
@@ -26,22 +18,4 @@ function populateDropdown(jsonData) {
         option.textContent = entity.name;
         dropdown.appendChild(option);
     });
-}
-
-
-window.onload = function () {
-    document.getElementById("create").addEventListener("click", function () {
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", document.URL+"/api/template");
-        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-        // const body = JSON.stringify({});
-        xhr.onload = () => {
-            if (xhr.status == 201) {
-                console.log(JSON.parse(xhr.responseText));
-            } else {
-                console.log(`Error: ${xhr.status}`);
-            }
-        };
-        xhr.send();
-    })
 }
