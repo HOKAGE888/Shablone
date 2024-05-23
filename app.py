3dbbbe427880706b9897ebc01fa1016ea2dd3362
HOKAGE888 <gozhevaKatya@yandex.ru>
2024-05-23 19:37:19 +0300

    Сделано: - Генерация изображений по шаблону - Архивация сгенерированных изображений
    Нужно:
    - скачивание в браузере
    - тени
    - подфильтр

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, abort, jsonify, send_file
-from models import Brand, ProductSubtype, ProductType, Template, Image, MetalType
+from models import Brand, ProductSubtype, ProductType, Template, Image, MetalType, Product
 import os
+import zipfile
 import subprocess
-import tempfile
+import requests
+import shutil
 from flask import json
 
 json.provider.DefaultJSONProvider.ensure_ascii = False
@@ -27,10 +29,13 @@ def hello():
 
 def generate_cmd_by_image(entity: dict):
 
-  try:
-    img_name = os.path.join('images', f'{int(entity["id"])}.png')
-  except:
-    img_name = os.path.join('image', entity["id"])
+  if entity['type'] == 'image':
+    try:
+      img_name = os.path.join('images', f'{int(entity["id"])}.png')
+    except:
+      img_name = os.path.join('image', str(entity["id"]))
+  elif entity['type'] == 'product':
+    img_name = os.path.join('products', f'{int(entity["id"])}.png')
 
 
   cmd = '-draw "image over '
@@ -63,7 +68,7 @@ def generate_template(template: Template):
 
   for entity in params['entities']:
     
-    if entity['type'] == 'image':
+    if entity['type'] in ['image', 'product']:
       cmd += generate_cmd_by_image(entity)
     elif entity['type'] == 'text':
       cmd += generate_cmd_by_text(entity)
@@ -73,8 +78,8 @@ def generate_template(template: Template):
 
   cmd += os.path.join(os.getcwd(), path, 'result.png').replace("\\", "\\\\")
   subprocess.check_output(cmd)
-  template.imagemagick = cmd
-  template.save()
+  return cmd
+
 
 
 # AAAAAAAAAAAAAAAAAAAAAAPPPPPPPPPPPPPPPPPPPPPPPPPPIIIIIIIIIIIIIIIIIIIIIII
@@ -174,6 +179,104 @@ def get_template(template_id):
 
   return template.json, 200
 
+def zip_folder(folder_path, zip_path):
+    # Создаем объект zip файла
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        # Проходим по всем директориям и файлам в указанной папке
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                # Получаем полный путь к файлу
+                file_path = os.path.join(root, file)
+                # Получаем относительный путь файла к архивируемой папке
+                arcname = os.path.relpath(file_path, start=folder_path)
+                # Добавляем файл в архив
+                zipf.write(file_path, arcname)
+
+def move_file(src_file, dest_dir):
+    # Проверяем, существует ли исходный файл
+    if not os.path.isfile(src_file):
+        print(f'Исходный файл не существует: {src_file}')
+        return
+
+    # Проверяем, существует ли каталог назначения
+    if not os.path.isdir(dest_dir):
+        print(f'Каталог назначения не существует: {dest_dir}')
+        return
+
+    # Получаем имя файла из пути исходного файла
+    file_name = os.path.basename(src_file)
+
+    # Полный путь к новому файлу в каталоге назначения
+    dest_file = os.path.join(dest_dir, file_name)
+
+    try:
+        # Перемещаем файл
+        shutil.move(src_file, dest_file)
+    except Exception as e:
+        print(f'Ошибка при перемещении файла: {e}')
+
+def get_product_from_entities(entities: list):
+  for entity in entities:
+    if entity['type'] == 'image' and entity['id'] == 'product.png':
+      return entity
+
+def rename_file(src_file, new_name):
+    # Проверяем, существует ли исходный файл
+    if not os.path.isfile(src_file):
+        print(f'Исходный файл не существует: {src_file}')
+        return
+
+    # Получаем директорию исходного файла
+    dir_name = os.path.dirname(src_file)
+
+    # Полный путь к новому файлу
+    new_file_path = os.path.join(dir_name, new_name)
+
+    try:
+        # Переименовываем файл
+        os.rename(src_file, new_file_path)
+    except Exception as e:
+        print(f'Ошибка при переименовании файла: {e}')
+
+@app.route('/api/template/<int:template_id>/zip/', methods=['GET'])
+def get_template_zip(template_id):
+  template = Template.get_by_id(template_id)
+  products = Product.filter(
+    product_subtype=template.product_subtype,
+    brand=template.brand,
+    metal_type=template.metal_type
+  )
+
+  params: dict = json.loads(template.json)
+  entity = get_product_from_entities(params['entities'])
+  entity['type'] = 'product'
+  project_path = os.path.join(os.getcwd(), 'projects', f'{template.id}')
+  
+  for product in products:
+    response = requests.get(product.url)
+    if response.status_code != 200:
+      return jsonify({'error': f'Не удалось скачать {product.url}'}), 404
+    
+    image_path = os.path.join(os.getcwd(), 'products', f'{product.id}.png')
+    with open(image_path, 'wb') as file:
+      file.write(response.content)
+    
+    entity['id'] = product.id
+    template.json = str(params).replace("'",'"')
+
+    generate_template(template)
+    rename_file(
+      os.path.join(project_path, 'result.png'),
+      os.path.join(project_path, f'{product.id}.png'),
+    )
+  
+  zip_file_path = os.path.join(os.getcwd(), 'projects', f'{template.id}.zip')
+  zip_folder(project_path, zip_file_path)
+  move_file(zip_file_path, project_path)      
+  
+  return jsonify({'debug': 'debug'}), 200
+
+
 @app.route('/api/template/<int:template_id>/image/', methods=['GET'])
 def get_template_image(template_id):
   
@@ -182,14 +285,15 @@ def get_template_image(template_id):
     return jsonify({'error': 'Шаблон не найден'}), 400
 
   if template.imagemagick is None:
-    generate_template(template)
+    cmd = generate_template(template)
+    template.imagemagick = cmd
+    template.save()
 
   return send_file(
     os.path.join(os.getcwd(), 'projects', f'{template.id}', 'result.png'),
     mimetype='image/png', 
     as_attachment=False
   )
-
 
 @app.route('/api/image/', methods=['POST'])
 def upload_file():
@@ -211,11 +315,14 @@ def upload_file():
 @app.route('/api/image/<string:image_name>/', methods=['GET'])
 def get_image_by_name(image_name):
   """Отправляет пользователю изображение"""
+  
   return send_file(os.path.join('image',  image_name), mimetype='image/png', as_attachment=False)
+
 
 @app.route('/api/image/<int:image_id>/', methods=['GET'])
 def get_image_by_id(image_id):
   """Отправляет пользователю изображение"""
+  
   image = Image.get_or_none(id=image_id)
   if image is None:
     return jsonify({'error': 'Image not found'}), 404
@@ -225,8 +332,9 @@ def get_image_by_id(image_id):
 
 
 if __name__ == '__main__':
-  for path in ['images', 'projects']:
+  for path in ['images', 'projects', 'products']:
       if not os.path.exists(path):
         os.mkdir(path)
   
+
   app.run(debug=True)
